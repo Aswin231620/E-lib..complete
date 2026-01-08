8ef5d48e34f028bc24d4c75f846a77332d853f1a
Ai Use <useai2316@gmail.com>
2026-01-08 23:39:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9437309381-f1df8",
+  "appId": "1:461491360428:web:d8d40b1cf505cd87274509",
+  "apiKey": "AIzaSyCyBC2-rgRuqsIKlAJl34wELBhCk-4Zda4",
+  "authDomain": "studio-9437309381-f1df8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "461491360428"
+};
